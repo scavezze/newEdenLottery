c00895023a0c66f7ef61327a50bdc45dc75247c0
Nate Scavezze <nscavezze@clickfox.com>
2015-05-27 11:11:02 -0600

    header jump to

--- client/views/faq/faqs.js
+++ client/views/faq/faqs.js
@@ -6,3 +6,12 @@ Template.faqs.helpers({
         return Faqs.find({enabled: true},{sort: {createdAt: -1}})
     }
 });
+
+Template.faqsItemHeader.events({
+    'click .question-jump-to':function(e,tmpl) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("question-" + tmpl.data._id).offset().top
+        }, 600);
+    }
+});
